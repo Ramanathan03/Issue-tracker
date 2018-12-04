c83a4c343748abf6e1719cc782bc45c8c3b2afcf
Ramanathan Annes <ramanathanannes47@gmail.com>
2018-12-04 18:43:07 +0000

    added aws key

--- tracker/settings.py
+++ tracker/settings.py
@@ -136,7 +136,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires':'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl':'max-age=94608000',
+}
+AWS_STORAGE_BUCKET_NAME = 'issue-tracker-ps'
+AWS_S3_REGION_NAME = 'eu-west-2'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorages'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
@@ -144,6 +155,7 @@ STATICFILES_DIRS = [
 MEDIAFILES_LOCATION = 'media'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorages'
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
