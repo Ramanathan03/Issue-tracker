78b412a0228b4fec7ccd5c79afedd3cd5c7d8799
Ramanathan Annes <ramanathanannes47@gmail.com>
2018-12-04 14:30:19 +0000

    deploying to heroku

--- tracker/settings.py
+++ tracker/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import env
 import dj_database_url
+if os.path.exists("env.py"):
+    import env
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -26,7 +27,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'),
+                 os.environ.get('HOSTNAME'),
+                  ]
 
 
 # Application definition
@@ -81,7 +84,13 @@ WSGI_APPLICATION = 'tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
+if "DATABASE_URL" in os.environ:
+      DATABASES = {
+            'default':dj_database_url.parse(os.environ.get('DATABASE_URL'))
+      }
+else:
+    print("PostGres is not found so we going to use sqllite")
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
